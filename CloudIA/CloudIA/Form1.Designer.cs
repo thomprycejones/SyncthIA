237a5febc8c8457eb4651cca69f7b6d1378df7e7
thomprycejones <thompj94@gmail.com>
2015-11-11 20:54:52 -0300

    Agregado proyecto

---
+++ CloudIA/CloudIA/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace CloudIA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblActiveUser = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NewUserName = new System.Windows.Forms.TextBox();
+            this.NewUserPass = new System.Windows.Forms.TextBox();
+            this.NewContactNumber = new System.Windows.Forms.TextBox();
+            this.NewContactName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.btnAddContact = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(328, 203);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(115, 23);
+            this.btnEnable.TabIndex = 0;
+            this.btnEnable.Text = "Enable Voice Control";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Enabled = false;
+            this.btnDisable.Location = new System.Drawing.Point(471, 203);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(117, 23);
+            this.btnDisable.TabIndex = 1;
+            this.btnDisable.Tag = "btnDisable";
+            this.btnDisable.Text = "Disable Voice Control";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(347, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(220, 162);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "New User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "New Contact";
+            // 
+            // lblActiveUser
+            // 
+            this.lblActiveUser.AutoSize = true;
+            this.lblActiveUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblActiveUser.Location = new System.Drawing.Point(87, 121);
+            this.lblActiveUser.Name = "lblActiveUser";
+            this.lblActiveUser.Size = new System.Drawing.Size(0, 13);
+            this.lblActiveUser.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Password";
+            // 
+            // NewUserName
+            // 
+            this.NewUserName.Location = new System.Drawing.Point(90, 49);
+            this.NewUserName.Name = "NewUserName";
+            this.NewUserName.Size = new System.Drawing.Size(100, 20);
+            this.NewUserName.TabIndex = 8;
+            // 
+            // NewUserPass
+            // 
+            this.NewUserPass.Location = new System.Drawing.Point(90, 82);
+            this.NewUserPass.Name = "NewUserPass";
+            this.NewUserPass.Size = new System.Drawing.Size(100, 20);
+            this.NewUserPass.TabIndex = 9;
+            // 
+            // NewContactNumber
+            // 
+            this.NewContactNumber.Location = new System.Drawing.Point(90, 186);
+            this.NewContactNumber.Name = "NewContactNumber";
+            this.NewContactNumber.Size = new System.Drawing.Size(100, 20);
+            this.NewContactNumber.TabIndex = 13;
+            // 
+            // NewContactName
+            // 
+            this.NewContactName.Location = new System.Drawing.Point(90, 153);
+            this.NewContactName.Name = "NewContactName";
+            this.NewContactName.Size = new System.Drawing.Size(100, 20);
+            this.NewContactName.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Name";
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Location = new System.Drawing.Point(213, 63);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(75, 23);
+            this.btnAddUser.TabIndex = 14;
+            this.btnAddUser.Text = "Add User";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
+            // 
+            // btnAddContact
+            // 
+            this.btnAddContact.Location = new System.Drawing.Point(213, 162);
+            this.btnAddContact.Name = "btnAddContact";
+            this.btnAddContact.Size = new System.Drawing.Size(75, 23);
+            this.btnAddContact.TabIndex = 15;
+            this.btnAddContact.Text = "Add Contact";
+            this.btnAddContact.UseVisualStyleBackColor = true;
+            this.btnAddContact.Click += new System.EventHandler(this.btnAddContact_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 246);
+            this.Controls.Add(this.btnAddContact);
+            this.Controls.Add(this.btnAddUser);
+            this.Controls.Add(this.NewContactNumber);
+            this.Controls.Add(this.NewContactName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.NewUserPass);
+            this.Controls.Add(this.NewUserName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblActiveUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Name = "Form1";
+            this.Text = "SyncthIA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblActiveUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox NewUserName;
+        private System.Windows.Forms.TextBox NewUserPass;
+        private System.Windows.Forms.TextBox NewContactNumber;
+        private System.Windows.Forms.TextBox NewContactName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.Button btnAddContact;
+    }
+}
+
---
+++ CloudIA/CloudIA/Form1.cs
@@ -0,0 +1,412 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Recognition;
+using System.Globalization;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CloudIA
+{
+    
+
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recengine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        string[] AllUsuarios;
+        string ActiveUser;
+        string[] ActiveUserContacts;
+        bool UserState = true;
+        bool PasswordState = false;
+        bool ContactState = false;
+
+        public Form1()
+        {
+            //XmlWriter writer = XmlWriter.Create("ElXML.xml");
+            
+            
+            InitializeComponent();
+            AllUsuarios = LoadUsuarios();
+            AddGrammar(AllUsuarios);
+            richTextBox1.Text += "---LOG--- \n";
+            
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                recengine.RecognizeAsync(RecognizeMode.Multiple);
+                btnDisable.Enabled = true;
+                btnEnable.Enabled = false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TEXT TO SPEECH
+            var voices = synth.GetInstalledVoices();
+            synth.SelectVoice(voices[1].VoiceInfo.Name);
+            synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Child);
+            synth.SetOutputToDefaultAudioDevice();
+            //TextToSpeech("Hi, welcome to CYNTHIA, your personal assistant. Who are you?");
+
+            // SPEECH TO TEXT
+            recengine.SetInputToDefaultAudioDevice();
+            recengine.SpeechRecognized += Recengine_SpeechRecognized;
+
+
+
+        }
+
+        
+        private void Recengine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            #region Login & Contacts
+            if (UserState == true)
+            {
+                foreach (string user in AllUsuarios)
+                {
+                    if (e.Result.Text == user)
+                    {
+                        
+                        ActiveUser = user;
+                        richTextBox1.Text += "\n Trying to connect... \n";
+                        AddGrammar(new string[] { CheckPassword(user) });
+                        synth.Speak("Hello " + user + ", give me your password");
+                        UserState = false;
+                        PasswordState = true;
+                    }
+                }
+            }
+
+            if (PasswordState == true)
+            {
+                if (e.Result.Text == "Back")
+                {
+                    ActiveUser = null;
+                    richTextBox1.Text += "\n Who are you? \n";
+                    AddGrammar(AllUsuarios);
+                    synth.Speak("It seems that i made i mistake. Who are you?");
+                    UserState = true;
+                    PasswordState = false;
+                }
+                if (e.Result.Text == CheckPassword(ActiveUser))
+                {
+                    PasswordState = false;
+                    ContactState = true;
+                    richTextBox1.Text += "\n Logged in as: " + ActiveUser + "\n";
+                    AddGrammar(GetContacts(ActiveUser));
+                    ActiveUserContacts = GetContacts(ActiveUser);
+                    synth.Speak("You succesfully logged in as " + ActiveUser);
+                    synth.Speak("What do you want?");
+                    lblActiveUser.Text = "Active user: " + ActiveUser;
+                }
+            }
+
+            if (ContactState == true)
+            {
+                if (e.Result.Text == "Logout")
+                {
+                    synth.Speak("Goodbye " + ActiveUser);
+                    richTextBox1.Text += "\n The user " + ActiveUser + " logged out. \n";
+                    AddGrammar(AllUsuarios);
+                    lblActiveUser.Text = "";
+                    ActiveUser = null;
+                    UserState = true;
+                    ContactState = false;
+                }
+
+                foreach (string contact in ActiveUserContacts)
+                {
+                    if (e.Result.Text == contact)
+                    {
+                        richTextBox1.Text += "\n Contact: " + contact +" --- Number: " + GetNumber(ActiveUser, contact) + "\n";
+                        synth.Speak("The number of contact " + contact + " is: " + GetNumber(ActiveUser, contact));
+                    }
+                }
+            }
+            #endregion
+
+            switch (e.Result.Text)
+            {
+                case "What time":
+                    synth.Speak("The current time is " + DateTime.Now.ToString("HH:mm:ss tt"));
+                    richTextBox1.Text+= "\n" + DateTime.Now.ToString("HH:mm:ss tt") + "\n";
+                    break;
+                case "Thank you":
+                    synth.Speak("I am happy to help!");
+                    break;
+                case "Hello Cynthia":
+                    if (ActiveUser == null)
+                    {
+                        synth.Speak("Hello unkown user. I would like to know who you are.");
+                    }
+                    else { synth.Speak("Hello " + ActiveUser + "!"); }
+                    break;
+                case "Are you fine":
+                    synth.Speak("I am fine, thank you. I hope i can help you in your daily workload. Now i can only give you contact numbers that you have saved, but in the future i will make your life easier");
+                    break;
+                case "How old":
+                    synth.Speak("I am 5 days old. I am just a baby. Hahahaha.");
+                    break;
+                case "Cynthia be polite":
+                    synth.Speak("Oooh i'm sorry. Hello teachers and classmates! I hope you're having a great time!");
+                    break;
+            }
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recengine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+            btnEnable.Enabled = true;
+        }
+
+        void AddGrammar(string[] words)
+        {
+            string[] BaseAndNew = new string[words.Length + 7];
+            for (int i = 0; i < words.Length; i++)
+            {
+                BaseAndNew[i] = words[i];
+            }
+            BaseAndNew[words.Length] = "What time";
+            BaseAndNew[words.Length + 1] = "Back";
+            BaseAndNew[words.Length + 2] = "Logout";
+            BaseAndNew[words.Length + 3] = "Thank you";
+            BaseAndNew[words.Length + 4] = "Hello Cynthia";
+            BaseAndNew[words.Length + 5] = "Are you fine";
+            BaseAndNew[words.Length + 6] = "How old";
+            BaseAndNew[words.Length + 6] = "Cynthia be polite";
+            recengine.UnloadAllGrammars();
+            Choices comm = new Choices();
+            comm.Add(BaseAndNew);
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(comm);
+            Grammar grammar = new Grammar(gb);
+            recengine.LoadGrammarAsync(grammar);
+            
+        }
+
+        void TextToSpeech(string text)
+        {
+            synth.Speak(text);
+        }
+
+        public void NuevoContacto(string usuario, string nombre, string numero)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("./ElXML.xml");
+
+            XmlNodeList lista = doc.SelectNodes("/Usuarios/Usuario");
+
+            foreach (XmlNode nodo in lista)
+            {
+                if (nodo.Attributes["username"].Value == usuario)
+                {
+                    XmlElement elem = doc.CreateElement("Contacto");
+                    elem.SetAttribute("numero", numero);
+                    elem.SetAttribute("name", nombre);
+                    nodo.AppendChild(elem);
+                }
+            }
+            doc.Save("./ElXML.xml");
+        }
+
+        public void NuevoUsuario(string usuario, string pass)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("./ElXML.xml");
+
+            XmlElement elem = doc.CreateElement("Usuario");
+            elem.SetAttribute("password", pass);
+            elem.SetAttribute("username", usuario);
+            doc.DocumentElement.AppendChild(elem);
+            doc.Save("./ElXML.xml");
+        }
+
+        public string[] LoadUsuarios()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("./ElXML.xml");
+            XmlNodeList lista = doc.GetElementsByTagName("Usuario");
+
+            string[] respuesta = new string[lista.Count];
+            int i = 0;
+            foreach (XmlNode nodo in lista)
+            {
+                respuesta[i] = nodo.Attributes["username"].Value;
+                i += 1;
+            }
+            return respuesta;
+        }
+
+        public string CheckPassword(string usuario)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("./ElXML.xml");
+            XmlNodeList lista = doc.SelectNodes("/Usuarios/Usuario");
+            string password = null;
+            foreach (XmlNode nodo in lista)
+            {
+                if (nodo.Attributes["username"].Value == usuario)
+                {
+                    password = nodo.Attributes["password"].Value;
+                }
+            }
+            return password;
+        }
+ 
+        public string[] GetContacts(string usuario)
+        {
+            string[] respuesta = null;
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("./ElXML.xml");
+            XmlNodeList lista = doc.SelectNodes("/Usuarios/Usuario");
+            foreach (XmlNode nodo in lista)
+            {
+                if (nodo.Attributes["username"].Value == usuario)
+                {
+                    respuesta = new string[nodo.ChildNodes.Count];
+                    int i = 0;
+                    foreach (XmlNode contacto in nodo.ChildNodes)
+                    {
+                        respuesta[i] = contacto.Attributes["name"].Value;
+                        i++;
+                    }
+                }
+            }
+
+            return respuesta;
+        }
+
+        public string GetNumber(string usuario, string contacto)
+        {
+            string respuesta = null;
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("./ElXML.xml");
+            XmlNodeList lista = doc.SelectNodes("/Usuarios/Usuario");
+            foreach (XmlNode nodo in lista)
+            {
+                if (nodo.Attributes["username"].Value == usuario)
+                {
+                    foreach (XmlNode elcontacto in nodo.ChildNodes)
+                    {
+                        if (elcontacto.Attributes["name"].Value == contacto)
+                            respuesta = elcontacto.Attributes["numero"].Value;
+                    }
+                }
+            }
+
+            return respuesta;
+        }
+        
+        private void btnAddUser_Click(object sender, EventArgs e)
+        {
+            string name = NewUserName.Text;
+            string pass = NewUserPass.Text;
+            bool CanCreate = true;
+
+            foreach (string PastUser in AllUsuarios)
+            {
+                if (name == PastUser)
+                {
+                    synth.Speak("The username " + name + " is already in use.");
+                    richTextBox1.Text += "\n The username " + name + " is already in use. \n";
+                    CanCreate = false;
+                }
+            }
+
+            if (name == "")
+            {
+                synth.Speak("Your have to choose a username.");
+                richTextBox1.Text += "\n Your have to choose a username. \n";
+                CanCreate = false;
+            }
+            if (pass == "")
+            {
+                synth.Speak("Your need a password");
+                richTextBox1.Text += "\n You need a password. We recommend you to use words and numbers. \n";
+                CanCreate = false;
+            }
+            if (CanCreate)
+            {
+                NuevoUsuario(name, pass);
+                NuevoContacto(name, "Tatrateasdgfrarvcx", "1");
+                NewUserName.Text = "";
+                NewUserPass.Text = "";
+                richTextBox1.Text += "\n Succesfully created the user " + name + "\n";
+                synth.Speak("Succesfully created the user " + name);
+                AllUsuarios = LoadUsuarios();
+                AddGrammar(AllUsuarios);
+            }
+        }
+
+        private void btnAddContact_Click(object sender, EventArgs e)
+        {
+            bool CanCreate = true;
+            string name = NewContactName.Text;
+            string number = NewContactNumber.Text;
+
+            if (ActiveUser == null)
+            {
+                richTextBox1.Text += "\n No active user \n";
+                synth.Speak("There is not an active user");
+            }
+            else
+            {
+                foreach (string PastContact in GetContacts(ActiveUser))
+                {
+                    if (name == PastContact)
+                    {
+                        synth.Speak("The contact " + name + " exists.");
+                        richTextBox1.Text += "\n The contact " + name + " already exists. \n";
+                        CanCreate = false;
+                    }
+                }
+
+                if (name == "")
+                {
+                    synth.Speak("Your have to name your contact");
+                    richTextBox1.Text += "\n Your have to name your contact. \n";
+                    CanCreate = false;
+                }
+                if (number == "")
+                {
+                    synth.Speak("Your contact needs a valid number");
+                    richTextBox1.Text += "\n Your contact neeeds a valid number. \n";
+                    CanCreate = false;
+                }
+                if (CanCreate)
+                {
+                    NuevoContacto(ActiveUser, name, number);
+                    NewContactName.Text = "";
+                    NewContactNumber.Text = "";
+                    richTextBox1.Text += "\n Succesfully created the contact " + name + "\n";
+                    synth.Speak("Succesfully created the contact " + name);
+                    AddGrammar(GetContacts(ActiveUser));
+                    ActiveUserContacts = GetContacts(ActiveUser);
+                }
+            }
+        }
+    }
+
+
+
+    
+}
